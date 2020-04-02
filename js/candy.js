2a8719aeda90f3490927b112b05d70485a9cf3fb
Shaneth <shanethp@gmail.com>
2020-04-01 23:30:15 -0400

    initial commit

---
+++ js/candy.js
@@ -0,0 +1,15 @@
+$("#coverbutton").click(function(){
+    $(".cover").fadeOut(800)
+
+    $( ".logo-top" ).addClass("nooffset");
+    $( ".logo-bottom" ).addClass("nooffset");
+    $( ".body" ).addClass("bodycanscroll");
+    setTimeout(videosIn(),1500);
+});
+
+function videosIn(){
+    $( ".video-l" ).addClass("nooffset");
+    $( ".video-r" ).addClass("nooffset");
+    $( ".candyoffset-l" ).addClass("nooffset");
+    $( ".candyoffset-r" ).addClass("nooffset");
+}
