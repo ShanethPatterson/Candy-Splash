0687eb6b442da13d27cd3f80a57b6d335c0df1e6
Shaneth <shanethp@gmail.com>
2020-04-07 13:56:36 -0400

    rewrote index and pushed newindex

--- js/candy.js
+++ js/candy.js
@@ -18,7 +18,21 @@ function bodyOnLoad(){
     setTimeout(function(){$( ".welcomebutton").addClass("welcomebuttonloaded");},800);
     $("#loadingtext").fadeOut(500);
     //document.getElementById("cover").style.backgroundColor="rgba(0,0,0,0.6)";
+    createVideoFrames();
 }
+
+function createVideoFrames() {
+        var div, n,
+            v = document.getElementsByClassName("youtube-player");
+        for (n = 0; n < v.length; n++) {
+            div = document.createElement("div");
+            div.setAttribute("data-id", v[n].dataset.id);
+            div.innerHTML = labnolThumb(v[n].dataset.id);
+            div.onclick = labnolIframe;
+            v[n].appendChild(div);
+        }
+    }
+
 
 /* Light YouTube Embeds by @labnol */
 /* Web: http://labnol.org/?p=27941 */
@@ -36,6 +50,8 @@ document.addEventListener("DOMContentLoaded",
         }
     });
 
+
+
 function labnolThumb(id) {
     var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
         play = '<div class="play"></div>';
@@ -44,9 +60,10 @@ function labnolThumb(id) {
 
 function labnolIframe() {
     var iframe = document.createElement("iframe");
-    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
-    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("allowfullscreen", "1");
+    iframe.setAttribute("allow","accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
+    var embed = "https://www.youtube.com/embed/ID?controls=0";
+    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     this.parentNode.replaceChild(iframe, this);
 }
